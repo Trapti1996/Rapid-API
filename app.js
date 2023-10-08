10907afd866988619abae145802a95b807248ab6
Hitesh Bhawsar <hiteshbhawsar@Togopools-MacBook-Pro-2.local>
2023-10-08 18:52:03 +0530

    health check and post method

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const morgan=require('morgan');
+const bodyParser      = require('body-parser');
 const app = express();
 const port = 8000;
 
 app.use(morgan('dev'));
 
+app.use(bodyParser.json());
+
 ///  endpoint - /  ==> handled by defalut call of GET
 app.get('/', (req, res) => {
     return res.end('Server is up');
+})
+
+
+app.post('/helth_check', (req, res) => {
+    return res.send(req.body.customers);
 })
 
 app.listen(port, (req, res) => {
